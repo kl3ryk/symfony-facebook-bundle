7aa6e5c2cf1ebb6f6d04339aa8a8a9fd8780c70c
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2013-07-08 17:22:01 +0200

    updated FacebookExtensionTrait to match FacebookExtensionInterface

--- FacebookExtensionTrait.php
+++ FacebookExtensionTrait.php
@@ -2,7 +2,6 @@
 
 namespace Laelaps\Bundle\Facebook;
 
-use Laelaps\Bundle\Facebook\DependencyInjection\FacebookExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
@@ -14,21 +13,19 @@ trait FacebookExtensionTrait
      * Optionally prefix imported configuration with given string.
      *
      * @param array $config
-     * @param \Laelaps\Bundle\Facebook\DependencyInjection\FacebookExtension $extension
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
      * @return null|string
      */
-    public function getFacebookConfigurationPrefix(array $config, FacebookExtension $extension, ContainerBuilder $container) {}
+    public function getFacebookConfigurationPrefix(array $config, ContainerBuilder $container) {}
 
     /**
      * Tell whether to strip dependency injection configuration or not.
      *
-     * @param \Laelaps\Bundle\Facebook\DependencyInjection\FacebookExtension $extension
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
      * @return bool if true then strip dependencyinjection configuration section
      * @see \Laelaps\Bundle\Facebook\Configuration\FacebookApplication::addFacebookAdapterSection
      */
-    public function getFacebookApplicationConfigurationOnly(FacebookExtension $extension, ContainerBuilder $container)
+    public function getFacebookApplicationConfigurationOnly(ContainerBuilder $container)
     {
         return true;
     }
